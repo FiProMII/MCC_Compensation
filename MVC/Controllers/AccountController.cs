0229f15ea90b34ec0c1d0d505bda496944cb8987
Randika Nurfaizi <44109733+dikanurfaizi@users.noreply.github.com>
2021-03-17 16:59:01 +0700

    -- Add MVC

---
+++ MVC/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Controllers
+{
+    public class AccountController : BaseController<Account, string>
+    {
+
+    }
+}
