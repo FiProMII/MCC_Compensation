7cf05f379b37f22707611853c65f78fa1943338d
Randika Nurfaizi <44109733+dikanurfaizi@users.noreply.github.com>
2021-03-17 14:12:06 +0700

    -- JWT & Middleware

---
+++ MCC_Compensation/ViewModels/LoginVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModels
+{
+    public class LoginVM
+    {
+        [DataType(DataType.EmailAddress)]
+        public string Email { set; get; }
+
+        [DataType(DataType.Password)]
+        public string Password { set; get; }
+        public string EmployeeName { get; set; }
+        public IEnumerable<string> Roles { set; get; }
+
+        public string RoleName { set; get; }
+
+    }
+}
