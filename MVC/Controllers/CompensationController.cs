0fa6ab46a8340bdd0431913a25e859195920c899
Randika Nurfaizi <44109733+dikanurfaizi@users.noreply.github.com>
2021-03-25 19:02:06 +0700

    -- View

--- MVC/Controllers/CompensationController.cs
+++ MVC/Controllers/CompensationController.cs
@@ -10,6 +10,5 @@ namespace MVC.Controllers
 {
     public class CompensationController : BaseController<Compensation, int>
     { 
-
     }
 }
--- MVC/Controllers/DepartmentController.cs
+++ MVC/Controllers/DepartmentController.cs
@@ -10,6 +10,5 @@ namespace MVC.Controllers
 {
     public class DepartmentController : BaseController<Department, int>
     {
-
     }
 }
