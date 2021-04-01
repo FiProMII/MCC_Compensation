f1d36747c209352a54474280c12da9c6324aec8e
Randika Nurfaizi <44109733+dikanurfaizi@users.noreply.github.com>
2021-04-01 11:57:06 +0700

    -- Edit SP Status

--- All Trigger & Procedure.sql
+++ All Trigger & Procedure.sql
@@ -90,17 +90,18 @@
 
 	-- Get Request List --
 	CREATE OR ALTER PROCEDURE [dbo].[SP_RetrieveDataStatus]
-		@Status nvarchar(max)
+		@Status nvarchar(max),
+		@NIK nvarchar(max)
 	AS
 	BEGIN
 	SELECT creq.RequestID, emp.EmployeeName, emp.JoinDate, (SELECT EmployeeName FROM TB_M_Employee WHERE NIK = (SELECT ManagerNIK FROM TB_M_Employee WHERE EmployeeName = emp.EmployeeName)) AS [Manager], comp.CompensationName, creq.EventDate, creq.RequestDate FROM TB_M_Employee emp
-JOIN TB_M_Position pos ON emp.PositionID = pos.PositionID
-JOIN TB_M_Department dep ON pos.DepartmentID = dep.DepartmentID
+	JOIN TB_M_Position pos ON emp.PositionID = pos.PositionID
+	JOIN TB_M_Department dep ON pos.DepartmentID = dep.DepartmentID
 	JOIN TB_T_CompensationRequest creq ON emp.NIK = creq.NIK 
 	JOIN TB_M_Compensation comp ON creq.CompensationID = comp.CompensationID
 	JOIN TB_T_Approval app ON creq.RequestID = app.RequestID
-	JOIN TB_M_Status st ON app.StatusID = st.StatusID WHERE app.StatusID = (SELECT s.StatusID FROM TB_M_Status s WHERE s.StatusName  LIKE '%Pendin%') AND
-	app.DepartmentID = (SELECT d.DepartmentID FROM TB_M_Employee e JOIN TB_M_Position p ON e.PositionID = p.PositionID JOIN TB_M_Department d ON p.DepartmentID = d.DepartmentID WHERE e.ManagerNIK = '117108') GROUP BY creq.RequestID,emp.EmployeeName,emp.joinDate,comp.CompensationName,creq.EventDate,creq.RequestDate
+	JOIN TB_M_Status st ON app.StatusID = st.StatusID WHERE app.StatusID = (SELECT s.StatusID FROM TB_M_Status s WHERE s.StatusName  LIKE '%'+@Status+'%') AND
+	app.DepartmentID = (SELECT d.DepartmentID FROM TB_M_Employee e JOIN TB_M_Position p ON e.PositionID = p.PositionID JOIN TB_M_Department d ON p.DepartmentID = d.DepartmentID WHERE e.ManagerNIK = @NIK) GROUP BY creq.RequestID,emp.EmployeeName,emp.joinDate,comp.CompensationName,creq.EventDate,creq.RequestDate
 	END
 	GO
 
